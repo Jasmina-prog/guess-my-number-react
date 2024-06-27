cc1006d60d3f8419908e83e888cc84307a350e7a
Jasmina ilya <islomovajasmina8@gmail.com>
2024-06-27 15:46:36 +0300

    4th commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,62 @@
 import './App.css'
 import { BelowPart } from './BelowPart'
 import { UpperPart } from './UpperPart'
+import Button from "./Button"
+import "./UpperPart.scss"
+import { useRef } from "react"
+import { useState } from "react"
+import "./UpperPart.scss"
+
 
 function App() {
 
+    const secretNumb = useRef()
+    console.log(secretNumb);
+
+    const [secretNumber, setSecretNumber] = useState(Math.trunc(Math.random()*20)+1)
+    console.log(secretNumber);
+    const testFunc=()=>{
+        console.log('this too');
+    }
+    const input = useRef()
+    console.log(secretNumber);
+    const checkNumber=()=>{
+        console.log(input.current.value);
+    }
   return (
     <>
-      <UpperPart/>
-      <BelowPart/>
+
+      <section className="upperpart">
+
+            <div className="wrapper1">
+            <Button title="Again!" checkFn={testFunc}/>
+            <h3>(Between 1 and 20)</h3>
+            </div>
+
+
+            <div className="wrapper2">
+                <h1>Guess My Number!</h1>
+                <div className="secretnumber" ref={secretNumb}>?</div>
+            </div>
+
+        </section>
+
+
+      <section className="belowpart">
+
+            <div className="wrapper3">
+                <input type="text" ref={input} />
+                <Button title="Check!" checkFn={checkNumber}/>
+            </div>
+
+
+            <div className="wrapper4">
+                <h2 className="message">Start guessing...</h2>
+                <h2>⭐Score: <span className="score">20</span> </h2>
+                <h2>🔥Highscore: <span className="highscore">0</span> </h2>
+            </div>
+
+        </section>
     </>
   )
 }
