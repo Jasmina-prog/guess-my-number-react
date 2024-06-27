7fed869b52082973ab2ff4c1d14e5a67c33c7f87
Jasmina ilya <islomovajasmina8@gmail.com>
2024-06-27 21:38:26 +0300

    7th commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Button from "./Button"
 import "./UpperPart.scss"
 import { useRef } from "react"
 import { useState } from "react"
-import "./UpperPart.scss"
+import "./BelowPart.scss"
 
 
 function App() {
@@ -12,17 +12,33 @@ function App() {
 
     const [secretNumber, setSecretNumber] = useState(Math.trunc(Math.random()*20)+1)
     const [isSuccess, setIsSeccess] = useState(false)
+    const [isLow, setIsLow] = useState(false)
+    const [isHigh, setIsHigh] = useState(false)
+    let [score, setScore] = useState(20)
+
     const testFunc=()=>{
         setSecretNumber(Math.trunc(Math.random()*20)+1)
+        setIsSeccess(false)
+        setIsLow(false)
+        setIsHigh(false)
+        input.current.value = ""
+        setScore(score = 20)
     }
+
     const input = useRef()
     const checkNumber=()=>{
           
         if(input.current.value == secretNumber){
           setIsSeccess(true)
-          
-        } else{
-          console.log(false)
+          input.current.value = ""
+        } else if(input.current.value < secretNumber){
+          setIsLow(true)
+          input.current.value = ""
+          setScore(score - 1)
+        } else if(input.current.value > secretNumber){
+          setIsHigh(true)
+          input.current.value = ""
+          setScore(score - 1)
         }
     }
   return (
@@ -54,8 +70,13 @@ function App() {
 
 
             <div className="wrapper4">
-                <h2 className="message">Start guessing...</h2>
-                <h2>⭐Score: <span className="score">20</span> </h2>
+                <h2 className="message">
+                  {isSuccess ? "You won 🎉" : "Start guessing..." &&
+                   isLow ? "Too Low" : "Start guessing..." &&
+                   isHigh ? "Too High" : "Start guessing..."
+                  } 
+                  </h2>
+                <h2>⭐Score: <span className="score">{score}</span> </h2>
                 <h2>🔥Highscore: <span className="highscore">0</span> </h2>
             </div>
 
